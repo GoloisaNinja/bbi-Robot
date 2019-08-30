798d1263a38c774f68a8fe49f807325458fd02d4
GoloisaNinja <47168633+GoloisaNinja@users.noreply.github.com>
2019-08-30 16:13:33 -0400

    form variable fix

--- contact.php
+++ contact.php
@@ -3,11 +3,10 @@
  $email = $_POST['emailType4'];
  $cat = $_POST['ideaCat'];
  $tag = $_POST['contactTag'];
- $idea = $_POST['contactIdea'];
+ $idea = $_POST['contactIdeaInput'];
  $formcontent="From: $name \n Email: $email \n Category: $cat \n Tags: $tag \n Idea: $idea";
  $recipient = "holidayninjastaff@gmail.com";
  $subject = "Bad Business Idea Submission";
  $mailheader = "From: $email \r\n";
  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
- echo "Thank You BoneSack!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
  ?>
