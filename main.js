7d270a1b0d5da85bdf094e037f253560a65e8ae3
Golodhrim <holidayninjastaff@gmail.com>
2019-08-31 00:19:04 -0400

    php fixes

--- main.js
+++ main.js
@@ -234,7 +234,7 @@ const pageCheck = function(){
     let contactCategory = ''
     let contactTagList = ''
     let contactIdeaVal = ''
-    const formData = document.querySelector("#form-two")
+    const formData = document.querySelector("#contactUs")
     formData.addEventListener('submit', function(e){
       contactName = e.target.elements.contactFN4.value
       contactEmail = e.target.elements.emailType4.value
