a7fbfc0ff5fa1eae52e7f7f885f39ae2a98dfcf3
Golodhrim <holidayninjastaff@gmail.com>
2019-09-03 19:26:52 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,16 @@
 <?php
  $name = $_POST['Name'];
+ $email = $_POST['Email'];
+ $catBad = $_POST['Category'];
+ $catTag = $_POST['TagList'];
+ $badIdea = $_POST['BadBusinessIdea']
+
+ $body = "From: $name \n Email: $email \n Category: $catBad \n Tags: $catTag \n Idea: $badIdea";
+ $recip = "holidayninjastaff@gmail.com";
+ $subject = "Bad Business Idea Submission";
+ $mailHeader = "From: $email \r\n";
+
+ mail($recip, $subject, $body, $mailHeader);
 ?>
  <!DOCTYPE html>
 <html lang="" dir="ltr">
