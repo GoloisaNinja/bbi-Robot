1f4d28ffdb5f7aafad28539e49d757552240e7af
Jon Collins <47168633+GoloisaNinja@users.noreply.github.com>
2019-09-25 14:36:32 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -132,7 +132,6 @@ No guarantees meatbag.
 <p></p>
 <script src=https://code.jquery.com/jquery-3.4.1.js type=text/javascript></script>
 <script src=https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js integrity=sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k crossorigin=anonymous></script>
-<script src=https://kit.fontawesome.com/1148c1f779.js></script>
 <script>$(document).ready(function(){setTimeout(function(){console.log("waited 10 secs");$("body").css("opacity",1)},100)});</script>
 <script src=assetMain.js></script>
 </body>
